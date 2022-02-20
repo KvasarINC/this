4abb8f2d21aeeec0527656144984f6038a9cf3d2
KvasarINC <99033916+KvasarINC@users.noreply.github.com>
2022-02-20 22:40:09 +0200

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,20 @@
+package com.learning;
+
+public class Test extends Robots{
+
+   // private String name;
+
+    public void say() {
+        System.out.println("Hello, user!");
+    }
+    //}
+
+   // public void setName(String Name) {
+      //  name = Name;
+    //}
+
+    //public void PrintParament() {
+      //  System.out.println("My name is: " + getName());
+    //}
+
+}
