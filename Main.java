4811ff8cccb4fd82263def1c250ea4578dd359d4
KvasarINC <99033916+KvasarINC@users.noreply.github.com>
2022-02-20 22:39:55 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.learning;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Robots robot1 = new Robots("Ya");
+        Test robot2 = new Test();
+
+        robot1.setName("Hoo!");
+
+        robot1.PrintParament();
+        robot2.PrintParament();
+
+
+    }
+
+}
